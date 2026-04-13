a844ad39c6ea93fbb0f6ad0c2b2b4ab9d656bd1a
MelissaLara-222 <melissalara935@gmail.com>
2026-04-13 15:55:32 -0600

    ajcbajdc

--- src/app/notificacionesprovedor/page.tsx
+++ src/app/notificacionesprovedor/page.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
-import styles from "./notificaciones.module.css";
+import styles from "./notificacionesprovedor.module.css";
 
 export default function Notificaciones() {
   const router = useRouter();
